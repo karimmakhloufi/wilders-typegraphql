16c634d5e21424219b29467d031c3768588d4732
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2021-09-12 17:10:20 +0200

    Created wilders graphql server for queries

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,83 @@
-console.log("hello world");
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Wilder {
+    name: String
+    city: String
+    skills: [Skill]
+  }
+
+  type Skill {
+    name: String
+    votes: Int
+  }
+
+  type Query {
+    wilders: [Wilder]
+  }
+`;
+
+const wilders = [
+  {
+    name: "Alain",
+    city: "Paris",
+    skills: [
+      {
+        name: "NodeJS",
+        votes: 3,
+      },
+    ],
+  },
+  {
+    name: "Bernadette",
+    city: "Paris",
+    skills: [
+      {
+        name: "NodeJS",
+        votes: 0,
+      },
+    ],
+  },
+  {
+    name: "Charles",
+    city: "Paris",
+    skills: [
+      {
+        name: "React",
+        votes: 2,
+      },
+    ],
+  },
+  {
+    name: "Delphine",
+    city: "Paris",
+    skills: [
+      {
+        name: "React",
+        votes: 3,
+      },
+    ],
+  },
+  {
+    name: "Edouard",
+    city: "Bordeaux",
+    skills: [
+      {
+        name: "VueJS",
+        votes: 0,
+      },
+    ],
+  },
+];
+
+const resolvers = {
+  Query: {
+    wilders: () => wilders,
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
